1656933078c346d44d09fd43cedd68ad31797fca
travjones <travjones0@gmail.com>
2016-05-06 15:56:42 -0400

    update'

--- client/src/pspa.js
+++ client/src/pspa.js
@@ -1,5 +1,15 @@
 import {shuffle} from './helper';
 
+// app container
+var appcontainer = document.getElementById("app");
+
+
+// data object to hold frequency of selection across stimuli
+var data = {}
+
+// array to hold src of each selection
+var choices = [];
+
 // stim pairs
 const stimpairs = [
   ["stimuli/1.gif", "stimuli/2.gif"],
@@ -21,10 +31,77 @@ for (var i = 0; i < stimpairs.length; i++) {
 console.log(randstimpairs);
 
 export default function pspa() {
-  var appcontainer = document.getElementById("app");
+  // render first pair
+  renderPair(0);
+  
+  var left = document.getElementById("left");
+  left.addEventListener("click", leftHandler);
+
+  var right = document.getElementById("right");
+  left.addEventListener("click", leftHandler);
+
+  // timer init
+  var timer = 0;
+
+  // timer/stimulus presentation logic
+  var pspaTask = setInterval(() => {
+    timer++;
+    console.log(timer);
+    
+    switch (timer) {
+      case 15:
+        renderPair(1);
+        break;
+      case 30:
+        renderPair(2);
+        break;
+      case 45:
+        renderPair(3);
+        break;
+      case 60:
+        renderPair(4);
+        break;
+      case 75:
+        renderPair(5);
+        break;
+      case 90:
+        console.log("done with pspa");
+        clearInterval(pspaTask);
+        break;
+    }
+
+  }, 1000);
   // TO-DO
   // write template -- use img src attribute to keep track of stim?
   // render randstimpairs
   // data -> freq of selection
 
+}
+
+function renderPair(i) {
+  let stim = randstimpairs[i];
+  let pair = stim,
+  taskHTML = `
+  <div id="sample-container" class="task-container row">
+    <div class="six columns">
+      <img src="${ pair[0] }" id="left">
+    </div>
+    <div class="six columns" id="right">
+      <img src="${ pair[1] }">
+    </div>
+  </div>`;
+
+  appcontainer.innerHTML = taskHTML;
+}
+
+function leftHandler() {
+  var left = document.getElementById("left");
+  var choice = left.getAttribute("src"); 
+  choices.push(choice);
+}
+
+function rightHandler() {
+  var right = document.getElementById("right");
+  var choice = right.getAttribute("src");
+  choices.push(choice);
 }
